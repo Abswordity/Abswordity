af7f37b12320725847eab8a67f808e88c6dd46f0
Adam Gordon <adamjgordon24@gmail.com>
2020-06-02 23:55:38 -0400

    Removed node_module folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import {GameBoard} from './Components/GameBoard/index'
 import './App.css';
 
-const fetchUsers = () => { fetch('http://localhost:1000/peoples')
+const fetchUsers = () => { fetch('http://localhost:1000/users')
 .then(response => response.json())
 .then(users => {
   console.log({users})
